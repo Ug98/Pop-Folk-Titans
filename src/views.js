680da0c5efd8ff8d8c047a9b5b869cb4a12ecc9f
Ug98 <iudjell98@gmail.com>
2024-07-05 16:29:35 +0300

    images and css templates

--- src/views.js
+++ src/views.js
@@ -1,6 +1,6 @@
 export const toHomeView = () => `
 <div id="home">
-  <h1>Mov(e)ster</h1>
+  <h1></h1>
   <div class="home-content">
     <p>Simple movie database app. You can:</p>
     <ul>
@@ -15,22 +15,22 @@ export const toHomeView = () => `
 
 export const toBulgariaView = () => `
 <div id="home">
-  <h1>Mov(e)ster</h1>
+  <h1>Singers</h1>
   <div class="home-content">
-    <p>Simple movie database app. You can:</p>
-    <ul>
-      <li>Bulgaria</li>
-      <li>Browse movies</li>
-      <li>Add and remove movies from favorites</li>
-      <li>Search for movies by title</li>
-    </ul>
+  <div class="images">
+<img src="imgs/nasko i milko.jpg" alt="nasko">
+<img src="imgs/preslava.jpg" alt="preslava">
+<img src="imgs/toni.jpg" alt="toni">
+<img src="imgs/galena.jpg" alt="galena">
+
+    </div>
   </div>
 </div>
 `;
 
 export const toSerbiaView = () => `
 <div id="home">
-  <h1>Mov(e)ster</h1>
+  <h1>Singers</h1>
   <div class="home-content">
     <p>Simple movie database app. You can:</p>
     <ul>
@@ -45,7 +45,7 @@ export const toSerbiaView = () => `
 
 export const toGreeceView = () => `
 <div id="home">
-  <h1>Mov(e)ster</h1>
+  <h1>Singers</h1>
   <div class="home-content">
     <p>Simple movie database app. You can:</p>
     <ul>
