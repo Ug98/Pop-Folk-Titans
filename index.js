0bb480124ac5370e9c15970d2b7570a2e5ac9142
Ug98 <iudjell98@gmail.com>
2024-07-05 17:38:54 +0300

    event listener for songs added

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ import { loadPage } from "./src/navigation.js";
 import { HOME } from "./src/constants.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    // add global listener
-    document.addEventListener('click', event => {
-      // nav events
-      if (event.target.classList.contains('nav-link')) {
-        console.log(event)
-        loadPage(event.target.getAttribute('data-page'));
-      }
-  
-    });
-  
-    loadPage(HOME);
-  });
+  // add global listener
+  document.addEventListener('click', event => {
+    // nav events
+    if (event.target.classList.contains('nav-link')) {
+      console.log(event)
+      loadPage(event.target.getAttribute('data-page'));
+    }
+   // image container events
+   const container = event.target.closest('.conteiner');
+   if (container) {
+     const key = container.getAttribute('data-key');
+     const audio = document.querySelector(`audio[data-key="${key}"]`);
+     if (audio) {
+       if (audio.paused) {
+         audio.currentTime = 0; // Rewind to the start
+         audio.play(); // Play the sound
+       } else {
+         audio.pause(); // Pause the sound
+       }
+     }
+   }
+ });
+
+  loadPage(HOME);
+});
+
--- src/views.js
+++ src/views.js
@@ -16,12 +16,27 @@ export const toHomeView = () => `
 export const toBulgariaView = () => `
 <div id="home">
   <h1>Singers</h1>
+  <p>Click on a picture to play a song</p>
   <div class="home-content">
   <div class="images">
-<img src="imgs/nasko i milko.jpg" alt="nasko">
+  <div class="conteiner" data-key="1">
+  <img src="imgs/nasko i milko.jpg" alt="nasko">
+</div>
+<div class="conteiner" data-key="2">
 <img src="imgs/preslava.jpg" alt="preslava">
+</div>
+<div class="conteiner" data-key="3">
 <img src="imgs/toni.jpg" alt="toni">
+</div>
+<div class="conteiner" data-key="4">
 <img src="imgs/galena.jpg" alt="galena">
+</div>
+
+  <audio data-key="1" src="songs/MILKO KALAYDZHIEV & NASKO MENTATA - BRAMCHALKA.mp3"></audio>
+  <audio data-key="2" src="songs/PRESLAVA - DYAVOLSKO ZHELANIE.mp3"></audio>
+  <audio data-key="3" src="songs/TONI STORARO - Otlichen (6).mp3"></audio>
+  <audio data-key="4" src="songs/GALENA - ZNAM DIAGNOZATA.mp3"></audio>
+  
 
     </div>
   </div>
@@ -33,10 +48,16 @@ export const toSerbiaView = () => `
   <h1>Singers</h1>
   <div class="home-content">
    <div class = "images">
-   <img src="imgs/indira.jpg" alt="indira">
-   <img src="imgs/ceca.png" alt="ceca">
-   <img src="imgs/mile.jpg" alt="mile">
-   <img src="imgs/shaban.jpg" alt="shaban">
+   <img data-key="5" src="imgs/indira.jpg" alt="indira">
+   <img data-key="6" src="imgs/ceca.png" alt="ceca">
+   <img data-key="7" src="imgs/mile.jpg" alt="mile">
+   <img data-key="8" src="imgs/shaban.jpg" alt="shaban">
+
+    <audio data-key="5" src="songs/at.mp3"></audio>
+  <audio data-key="6" src="songs/fr.mp3"></audio>
+  <audio data-key="7" src="songs/sr.mp3"></audio>
+  <audio data-key="8" src="songs/sr.mp3"></audio>
+
    </div>
   </div>
 </div>
