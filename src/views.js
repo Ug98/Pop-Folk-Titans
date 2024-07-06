b84d5c647bd3b7e45c5c747f319771bf4ec3fcc6
Ug98 <iudjell98@gmail.com>
2024-07-06 12:20:04 +0300

    some css changes and more songs added

--- src/views.js
+++ src/views.js
@@ -20,20 +20,20 @@ export const toBulgariaView = () => `
   <div class="home-content">
   <div class="images">
   <div class="conteiner" data-key="1">
+  <p>Milko & Nasko the Mint</p>
   <img src="imgs/nasko i milko.jpg" alt="nasko">
-  <p>Milko and Nasko the Mint</p>
 </div>
 <div class="conteiner" data-key="2">
-<img src="imgs/preslava.jpg" alt="preslava">
 <p>Preslava</p>
+<img src="imgs/preslava.jpg" alt="preslava">
 </div>
 <div class="conteiner" data-key="3">
-<img src="imgs/toni.jpg" alt="toni">
 <p>Toni Storaro</p>
+<img src="imgs/toni.jpg" alt="toni">
 </div>
 <div class="conteiner" data-key="4">
-<img src="imgs/galena.jpg" alt="galena">
 <p>Galena</p>
+<img src="imgs/galena.jpg" alt="galena">
 </div>
 
   <audio data-key="1" src="songs/MILKO KALAYDZHIEV & NASKO MENTATA - BRAMCHALKA.mp3"></audio>
@@ -54,20 +54,20 @@ export const toSerbiaView = () => `
   <div class="home-content">
    <div class = "images">
   <div class="conteiner" data-key="5">
+  <p>Indira Radic</p>
 <img src="imgs/indira.jpg" alt="indira">
-<p>Indira Radic</p>
 </div>
   <div class="conteiner" data-key="6">
+  <p>Ceca Velichkovich</p>
 <img src="imgs/ceca.png" alt="ceca">
-<p>Ceca Velichkovich</p>
 </div>
   <div class="conteiner" data-key="7">
+  <p>Mile Kitic</p>
 <img src="imgs/mile.jpg" alt="mile">
-<p>Mile Kitic</p>
 </div>
    <div class="conteiner" data-key="8">
+   <p>Shaban Shaulic</p>
 <img src="imgs/shaban.jpg" alt="shaban">
-<p>Shaban Shaulic</p>
 </div>
    
 
@@ -88,26 +88,47 @@ export const toGreeceView = () => `
   <div class="home-content">
   <div class = "images">
   <div class="conteiner" data-key="9">
+  <p>Vasilis Karras</p>
 <img src="imgs/vasilis.jpeg" alt="vasilis">
-<p>Vasilis Karras</p>
 </div>
   <div class="conteiner" data-key="10">
+  <p>Notis Sfakianakis</p>
 <img src="imgs/notis.jpg" alt="notis">
-<p>Notis Sfakianakis</p>
 </div>
   <div class="conteiner" data-key="11">
+  <p>Antique</p>
 <img src="imgs/antique.jpeg" alt="antique">
-<p>Antique</p>
 </div>
   <div class="conteiner" data-key="12">
+  <p>Keti Garbi</p>
 <img src="imgs/keti.jpg" alt="keti">
-<p>Keti Garbi</p>
 </div>
+  <div class="conteiner" data-key="13">
+  <p>Peggy Zina</p>
+<img src="imgs/zina.jpg" alt="antique">
+</div>
+  <div class="conteiner" data-key="14">
+  <p>Christos Kiryazis</p>
+<img src="imgs/Christos.webp" alt="Christos">
+</div>
+   <div class="conteiner" data-key="15">
+   <p>Despina Vandi</p>
+  <img src="imgs/vandi.jpg" alt="Vandi">
+  </div>
+  <div class="conteiner" data-key="16">
+  <p>Pantelis Pantelidis</p>
+  <img src="imgs/pantelis.jpg" alt="Pantelis">
+  </div>
 
+  <audio data-key="16" src="songs/pΠαντελής Παντελίδης - Γίνεται.mp3"></audio>
+  <audio data-key="15" src="songs/Na Ti Xerese.mp3"></audio>
+  <audio data-key="14" src="songs/Christos Kyriazis - Epimeno.mp3"></audio>
   <audio data-key="9" src="songs/Fenomeno.mp3"></audio>
   <audio data-key="10" src="songs/Νότης Σφακιανάκης Σώμα μου  Notis Sfakianakis Soma mou.mp3"></audio>
   <audio data-key="11" src="songs/Antique - Dinata Dinata (Official Video) 1999.mp3"></audio>
   <audio data-key="12" src="songs/esena mono  - kaiti garbi.mp3"></audio>
+  <audio data-key="13" src="songs/Peggy Zina   Eimai kala HD 1080p].mp3"></audio>
+  
 
     </div>
   </div>
